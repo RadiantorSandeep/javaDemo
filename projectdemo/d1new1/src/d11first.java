7bcfde9b123e484457d6938f33c200a73bea548c
Sandeep Kumar <147305849+RadiantorSandeep@users.noreply.github.com>
2024-06-08 01:16:47 +0530

    Add files via upload
    this is my second commit

---
+++ projectdemo/d1new1/src/d11first.java
@@ -0,0 +1,5 @@
+public class d11first {
+    public static void main(String[] args) {
+        System.out.println("first");
+    }
+}
---
+++ projectdemo/d2new2/src/d21first.java
@@ -0,0 +1,5 @@
+public class d21first {
+    public static void main(String[] args) {
+        System.out.println("hello first");
+    }
+}
