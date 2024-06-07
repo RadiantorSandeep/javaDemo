3c07035829cb735e4e55bef8b40fcbdb3b6cbd74
Sandeep Kumar <147305849+RadiantorSandeep@users.noreply.github.com>
2024-06-08 00:54:30 +0530

    Add files via upload
    my first project

---
+++ newproject/n1new1/src/n1first.java
@@ -0,0 +1,5 @@
+public class n1first {
+    public static void main(String[] args) {
+        System.out.println("abc");
+    }
+}
---
+++ newproject/n2new2/src/n2second.java
@@ -0,0 +1,5 @@
+public class n2second {
+    public static void main(String[] args) {
+        System.out.println("xyz");
+    }
+}
